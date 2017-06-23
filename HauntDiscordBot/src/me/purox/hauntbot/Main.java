a3f34271a356a7b6d9d922229972a8b34e27bbe1
Purox <purox90@web.de>
2017-06-24 00:53:56 +0200

    Main class update

--- HauntDiscordBot/src/me/purox/hauntbot/Main.java
+++ HauntDiscordBot/src/me/purox/hauntbot/Main.java
@@ -1,7 +1,35 @@
 package me.purox.hauntbot;
 
-/**
- * Created by Daniel on 6/24/2017.
- */
+import net.dv8tion.jda.core.AccountType;
+import net.dv8tion.jda.core.JDABuilder;
+import net.dv8tion.jda.core.OnlineStatus;
+import net.dv8tion.jda.core.entities.Game;
+import net.dv8tion.jda.core.exceptions.RateLimitedException;
+
+import javax.security.auth.login.LoginException;
+
 public class Main {
+
+    private static String TOKEN = "MzIwNTkwOTg0MTg4NjU3NjY0.DC8oKA.Go9tfCStJUEWSegScAYnSaIk3lk\n" ;
+
+    public static void main(String[] Args) {
+        JDABuilder builder = new JDABuilder(AccountType.BOT);
+
+        builder.setToken(TOKEN);
+        builder.setAutoReconnect(true);
+        builder.setStatus(OnlineStatus.DO_NOT_DISTURB);
+        builder.setGame(Game.of("@HauntNetwork"));
+
+        try {
+            builder.buildBlocking();
+        } catch (LoginException e) {
+            e.printStackTrace();
+        } catch (IllegalArgumentException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (RateLimitedException e) {
+            e.printStackTrace();
+        }
+    }
 }
